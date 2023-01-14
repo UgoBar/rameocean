1685a7064799ee96d8ec5789ed5c0a120413d8f9
UgoBar <ugo17190@gmail.com>
2023-01-14 14:18:42 +0100

    Coordinate -> add image for analytics

--- admin/controllers/CoordinateController.php
+++ admin/controllers/CoordinateController.php
@@ -5,10 +5,12 @@ require('../models/Coordinate.php');
 class CoordinateController extends DefaultController
 {
     private $coordinate;
+    private $media;
 
     public function __construct()
     {
-        $this -> coordinate = new Coordinate();
+        $this -> media = new Media();
+        $this -> coordinate = new Coordinate($this->media);
     }
 
     public function getCoordinates()
@@ -18,6 +20,8 @@ class CoordinateController extends DefaultController
         foreach ($coordinates as $coordinate) {
             if (isset($_POST['delete-coordinate-' . $coordinate['id']])) {
                 $id      = (int)$_POST['delete-coordinate-' . $coordinate['id']];
+                $picture = $_POST['picture-' . $coordinate['id']];
+                $this -> deleteFile($picture, 'coordinate');
                 $this -> coordinate -> delete($id);
                 header('Location:index.php?name=coordinates');
             }
@@ -34,6 +38,10 @@ class CoordinateController extends DefaultController
         $pageTitle = 'Ajouter une coordonnées';
         $id = array_key_exists('id',$_GET) ? (int)$_GET['id'] : null;
         $date = date("Y-m-d");
+        date_default_timezone_set('Europe/Paris');
+        $hour = date('H:i');
+        $hour = str_replace(":", "h", $hour);
+        $picture = '';
 
         // EDITION MODE
         if($id) {
@@ -44,6 +52,7 @@ class CoordinateController extends DefaultController
             $hour         = $coordinate['hour'];
             $latitude     = $coordinate['latitude'];
             $longitude    = $coordinate['longitude'];
+            $oldPicture   = $coordinate['picture'];
             $errors       = [];
 
             $pageTitle = "Modification de la coordonnées du $date";
@@ -53,27 +62,38 @@ class CoordinateController extends DefaultController
 
             // On hydrate les variables avec les données reçues du formulaire
             $date        = (isset($_POST['date'])) ? trim($_POST['date']) : $date;
-            $description = (isset($_POST['hour'])) ? trim($_POST['hour']) : '18h30';
+            $hour        = (isset($_POST['hour'])) ? trim($_POST['hour']) : '18h30';
             $latitude    = (isset($_POST['latitude'])) ? trim($_POST['latitude']) : 0;
             $longitude   = (isset($_POST['longitude'])) ? trim($_POST['longitude']) : 0;
+            $alt         = (isset($_POST['alt'])) ? trim($_POST['alt']) : '';
+            $oldPicture  = (isset($_POST['oldPicture'])) ? trim($_POST['oldPicture']) : null;
 
             // Gestion des erreurs
             if (empty($date))
                 $errors['date'] = 'Ce champ est obligatoire';
-            if (empty($description))
+            if (empty($hour))
                 $errors['description'] = 'Ce champ est obligatoire';
             if (empty($latitude))
                 $errors['latitude'] = 'Ce champ est obligatoire';
             if (empty($longitude))
                 $errors['longitude'] = 'Ce champ est obligatoire';
 
+            /** Upload du fichier et gestion d'erreur */
+            try {
+                $picture = $this -> uploadFile('picture', 'coordinate');
+            } catch (DomainException $e) {
+                $errors['picture'] = $e -> getMessage();
+            }
+
             if (empty($errors)) {
 
+                $picture = $this->keepOrReplacePicture($picture, $oldPicture, 'coordinate');
+
                 if (!$id) {
-                    $this -> coordinate -> add($date, $description, $latitude, $longitude);
+                    $this -> coordinate -> add($date, $hour, $latitude, $longitude, $picture, $alt);
                     $this -> addFlashBag("La coordonnée a bien été ajoutée", 'success');
                 } else {
-                    $this -> coordinate -> update($id, $date, $description, $latitude, $longitude);
+                    $this -> coordinate -> update($id, $date, $hour, $latitude, $longitude, $picture, $alt);
                     $this -> addFlashBag("La coordonnée a bien été mise à jour", 'success');
                 }
                 header('Location:index.php?name=coordinates');
@@ -87,6 +107,9 @@ class CoordinateController extends DefaultController
             'hour'        => $hour ?? '',
             'latitude'    => $latitude ?? '',
             'longitude'   => $longitude ?? '',
+            'picture'     => $picture,
+            'alt'         => $alt ?? '',
+            'oldPicture'  => $oldPicture ?? null,
             'errors'      => $errors ?? [],
         ]);
     }
--- models/Coordinate.php
+++ models/Coordinate.php
@@ -3,38 +3,48 @@
 class Coordinate
 {
     private $dbh;
+    private $media;
 
-    public function __construct()
+    public function __construct($media)
     {
         $this->dbh = dbConnexion();
+        $this->media = $media;
     }
 
-    public function add($date, string $hour, $lat, $lon)
+    public function add($date, string $hour, $lat, $lon, string $picture, string $alt)
     {
 
         $this->dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);
+        $lastInsertMediaId = $this->media->add($picture, $alt);
 
         $stm = $this->dbh->prepare (
-            "INSERT INTO ro_coordinate (date, hour, latitude, longitude)
-                    VALUES (:date, :hour, :lat, :lon)"
+            "INSERT INTO ro_coordinate (date, hour, latitude, longitude, media_id)
+                    VALUES (:date, :hour, :lat, :lon, :media_id)"
         );
 
         $stm->bindValue('date', $date);
         $stm->bindValue('hour', $hour);
         $stm->bindValue('lat', $lat);
         $stm->bindValue('lon', $lon);
+        $stm->bindValue('media_id', $lastInsertMediaId);
         $stm->execute();
 
         return $this->dbh->lastInsertId();
     }
 
-    public function update(int $coordinateId, $date, string $hour, $lat, $lon)
+    public function update(int $coordinateId, $date, string $hour, $lat, $lon, string $picture, string $alt)
     {
         $this->dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);
+        $mediaId = $this->findMediaId($coordinateId);
 
         $stm = $this->dbh->prepare("
+            UPDATE ro_media m SET m.picture = :picture, m.alt = :alt WHERE m.id = :mediaId;
             UPDATE ro_coordinate c SET c.date = :date, c.hour = :hour, c.latitude = :lat, c.longitude = :lon WHERE c.id = :coordinateId;
         ");
+
+        $stm->bindValue('mediaId', $mediaId, PDO::PARAM_INT);
+        $stm->bindValue('picture', $picture);
+        $stm->bindValue('alt', $alt);
 
         $stm->bindValue('coordinateId', $coordinateId, PDO::PARAM_INT);
         $stm->bindValue('date', $date);
@@ -43,18 +53,20 @@ class Coordinate
         $stm->bindValue('lon', $lon);
         $stm->execute();
 
-        $stm->execute();
-        return $this->dbh->lastInsertId();
+       return $this->dbh->lastInsertId();
     }
 
     public function delete(int $coordinateId)
     {
         $this->dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);
+        $mediaId = $this->findMediaId($coordinateId);
 
         $stm = $this->dbh->prepare("
             DELETE FROM ro_coordinate WHERE id = :coordinateId;
+            DELETE FROM ro_media where id = :mediaId;
         ");
         $stm->bindValue('coordinateId', $coordinateId);
+        $stm->bindValue('mediaId', $mediaId);
 
         $stm->execute();
     }
@@ -64,25 +76,49 @@ class Coordinate
         $this->dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
         $stm = $this->dbh->prepare("
-            SELECT *
+            SELECT c.id, c.media_id, c.date, c.hour, c.latitude, c.longitude, m.picture, m.alt
             FROM ro_coordinate c
-            ORDER BY c.date DESC;
+            LEFT JOIN ro_media m ON c.media_id = m.id
+            ORDER BY c.date DESC
         ");
 
         $stm->execute();
         return $stm->fetchAll();
     }
 
+    public function findLastOne()
+    {
+        $this->dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+        $stm = $this->dbh->prepare("
+            SELECT c.date, c.hour, c.latitude, c.longitude, m.picture, m.alt
+            FROM ro_coordinate c
+            LEFT JOIN ro_media m ON c.media_id = m.id
+            ORDER BY c.date DESC
+            LIMIT 1;
+        ");
+
+        $stm->execute();
+        return $stm->fetch();
+    }
+
     public function findById(int $id)
     {
         $stm = $this->dbh->prepare("
-            SELECT *
+            SELECT c.media_id, c.date, c.hour, c.latitude, c.longitude, m.picture, m.alt
             FROM ro_coordinate c
-            WHERE c.id = :id
+            LEFT JOIN ro_media m ON c.media_id = m.id
+            WHERE c.id = :id;
         ");
 
         $stm->bindValue('id', $id);
         $stm->execute();
         return $stm->fetch();
+    }
+
+    public function findMediaId(int $newsId)
+    {
+        $currentNews = $this->findById($newsId);
+        return $currentNews['media_id'];
     }
 }
